9951107cf17931623cf3c27180df662d5705c28d
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-13 19:50:06 -0300

    requirement 2

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import route from './routes/products.routes';
+import httpErrorMiddleware from './middlewares/httpError.middleware';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(route);
+app.use('/products', route);
+
+app.use(httpErrorMiddleware);
 
 export default app;
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import { validateName, validateAmount } from '../middlewares/validateProduct';
 
 const router = Router();
 
 const productsController = new ProductsController();
 
-router.get('/products', productsController.getAll);
+router.get('/', productsController.getAll);
+router.post('/', validateName, validateAmount, productsController.createProduct);
 
 export default router;
