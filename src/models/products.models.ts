f25709d0f059d155dd9d0db8acceb265130708d6
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-13 16:29:08 -0300

    class ProductsModel returns an array

--- src/models/products.models.ts
+++ src/models/products.models.ts
@@ -9,9 +9,11 @@ export default class ProductsModel {
   }
 
   public async getAll(): Promise<Product[]> {
+    // define a interface Product como tipo do retorno da função getAll e especifica que o resultado da função é uma Promise que encapsula um array de objetos do tipo Product
     const result = await this.connection
       .execute('SELECT * FROM Trybesmith.Products');
     const [rows] = result;
     return rows as Product[];
+    // a palavra reservada as que faz uma adaptação (cast) do objeto row que agora passa a esperar um array de objetos da interface Product
   }
 }
