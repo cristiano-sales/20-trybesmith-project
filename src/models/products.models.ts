43ebc47c8f1b74a45db5c14c17e68aa7959e1590
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-13 15:42:32 -0300

    implement model with class

---
+++ src/models/products.models.ts
@@ -0,0 +1,16 @@
+import { Pool } from 'mysql2/promise';
+
+export default class ProductsModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll() {
+    const result = await this.connection
+      .execute('SELECT * FROM Trybesmith.Products');
+    const [rows] = result;
+    return rows;
+  }
+}
