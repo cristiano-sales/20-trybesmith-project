f8bfcfaa814d2597b420fb9cb8a426f69942249c
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-14 20:29:22 -0300

    app use order router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import 'express-async-errors';
-import route from './routes/products.routes';
+import productsRouter from './routes/products.routes';
+import userRouter from './routes/user.route';
+import ordersRouter from './routes/orders.routes';
 import httpErrorMiddleware from './middlewares/httpError.middleware';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/products', route);
+app.use('/products', productsRouter);
+
+app.use('/users', userRouter);
+
+app.use('/orders', ordersRouter);
 
 app.use(httpErrorMiddleware);
 
