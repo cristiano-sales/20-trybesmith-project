13e87c0fca17d6eb760cdf469b1bac096bdacf7f
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-13 22:10:39 -0300

    requirement 03

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import 'express-async-errors';
 import route from './routes/products.routes';
 import httpErrorMiddleware from './middlewares/httpError.middleware';
+import userRouter from './routes/user.route';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', route);
+
+app.use('/users', userRouter);
 
 app.use(httpErrorMiddleware);
 
